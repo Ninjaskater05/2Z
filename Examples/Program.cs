ed648516fe2a4eac751dee830c957fa429a255c6
Muhammed Secic <muha3340@mail.hansenberg.dk>
2023-12-06 12:44:11 +0100

    C# exercise 6 (The solution)
    Write a C# Sharp program to print the output of the multiplication of three numbers entered by the user.
    Test Data:
    Input the first number to multiply: 2
    Input the second number to multiply: 3
    Input the third number to multiply: 6
    Expected Output:
    2 x 3 x 6 = 36

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,15 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int num1, num2, num3;
 
-Console.Write("\nWrite the first number: ");
-num1 = int.Parse(Console.ReadLine());
+Console.Write("\nInput the first number to multiply: ");
+num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("\nWrite the second number: ");
-num2 = int.Parse(Console.ReadLine());
+Console.Write("\nInput the second number to multiply: ");
+num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("\nWrite the third number: ");
-num3 = int.Parse(Console.ReadLine());
+Console.Write("\nInput the third number to multiply: ");
+num3 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(num2 * num2 * num3);
+int result = num1 * num2 * num3;
 
-Console.Read();
+Console.WriteLine("Output: {0} * {1} * {2} = {3}", num1, num2, num3, result);
