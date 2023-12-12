55b17da6f754a40e855549c7ac4bd36a4bba9f2e
Muhammed Secic <muha3340@mail.hansenberg.dk>
2023-12-12 09:48:51 +0100

    if statement C# exercises 8
    Write a C# Sharp program to find the largest of three numbers

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,17 +1,26 @@
-﻿float height;
+﻿float num1, num2, num3;
 
-Console.Write("Input your height (in cm): ");
-height = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
 
-if (height < 160)
+Console.Write("Input second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input third number: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
 {
-    Console.WriteLine("This person a dwarf");
+    if (num1 > num3)
+    {
+        Console.WriteLine("{0} is the biggest number", num1);
+    }
+    else
+    {
+        Console.WriteLine("{0} is the biggest number", num3);
+    }
 }
-else if (height >= 160 && height < 210)
+else
 {
-    Console.WriteLine("This person a normal height");
-}
-else 
-{
-    Console.WriteLine("WTF?");
+    Console.WriteLine("{0} is the biggest number", num2);
 }
