7e222d241a67d47681b17d1b8ba19da92aa207a0
Muhammed Secic <muha3340@mail.hansenberg.dk>
2023-12-12 08:42:57 +0100

    if statement C# exercises 1
    Started new C# exercise.
    
    Write a C# Sharp program to accept two integers and check whether they are equal or not

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,15 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-int num1, num2, num3;
+﻿int int1, int2;
 
-Console.Write("\nInput the first number to multiply: ");
-num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input first number: ");
+int1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("\nInput the second number to multiply: ");
-num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input second number: ");
+int2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("\nInput the third number to multiply: ");
-num3 = Convert.ToInt32(Console.ReadLine());
+if (int1 == int2)
+{
+    Console.WriteLine("{0} and {1} are equal", int1, int2);
+}   
 
-int result = num1 * num2 * num3;
-
-Console.WriteLine("Output: {0} * {1} * {2} = {3}", num1, num2, num3, result);
+else
+{
+    Console.WriteLine("{0} is not equal to {1}", int1, int2);
+}
