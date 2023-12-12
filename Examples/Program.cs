8d946a64a13fd751cb6d14f71b6577a54689ed0d
Muhammed Secic <muha3340@mail.hansenberg.dk>
2023-12-12 08:58:36 +0100

    if statement C# exercises 3
    Write a C# Sharp program to check whether a given number is positive or negative

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,15 +1,13 @@
-﻿int int1, remainder1;
+﻿int num;
 
-Console.Write("Input an integer to check whether number odd or even: ");
-int1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number to check if positive or negative: " );
+num = Convert.ToInt32(Console.ReadLine());
 
-remainder1 = int1 % 2;
-
-if (remainder1 == 0)
+if (num >= 0)
 {
-    Console.WriteLine ("{0} is an even integer", int1);
+    Console.WriteLine("{0} is a positive number", num);
 }
 else
 {
-    Console.WriteLine ("{0} is an odd integer", int1);
+    Console.WriteLine("{0} is a negative number", num);
 }
